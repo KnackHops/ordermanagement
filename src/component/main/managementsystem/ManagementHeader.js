1be6a7807af3feda6446745def96fc65ece565d4
unknown <aldrian.telan@gmail.com>
2022-07-23 03:24:39 +0800

    modal

--- src/component/main/managementsystem/ManagementHeader.js
+++ src/component/main/managementsystem/ManagementHeader.js
@@ -1,5 +1,7 @@
-import { Button, TextField, Autocomplete } from "@mui/material"
+import { Button, TextField, Autocomplete, Modal, Box } from "@mui/material"
+import { useState } from "react";
 import './ManagementHeader.scss';
+import ManagementNewOrderModal from "./ManagementNewOrderModal";
 
 const filterByOption = [
     "ORDER ID",
@@ -8,47 +10,74 @@ const filterByOption = [
     "PAYMENT STATUS"
 ]
 
+const InputLabelProps = {
+  shrink: true,
+}
+
+const boxStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    bgcolor: 'white',
+    padding: '4rem'
+}
+
 const ManagementHeader = () => {
-    const InputLabelProps = {
-      shrink: true,
+    const [ addOrderOpen, setAddOrderOpen ] = useState( false )
+
+    const handleAddOrderOpen = () => setAddOrderOpen( true )
+    const handleAddOrderClose = () => setAddOrderOpen( false )
+
+    const addNewOrderOpen = () => {
+        handleAddOrderOpen()
     }
 
     return (
-    <div className="section-header margin--top-standard fd">
-        <Button className="add-btn -big" variant="contained">Add New Order</Button>
-        <div className="header-fields fd">
-            <Autocomplete
-                id="filter_by"
-                autoHighlight
-                options={ filterByOption }
-                sx={ { width: "12.5rem" } }
-                renderInput={ params => 
+        <div className="section-header margin--top-standard fd">
+            <Button className="add-btn -big" type="button" variant="contained" onClick={ addNewOrderOpen }>Add New Order</Button>
+            <Modal 
+                open={addOrderOpen}
+                onClose={ handleAddOrderClose } >
+                    <Box sx={ boxStyle }>
+                        <ManagementNewOrderModal
+                            open={ addOrderOpen } 
+                            handleClose={ handleAddOrderClose } />
+                    </Box>
+            </Modal>
+            <div className="header-fields fd">
+                <Autocomplete
+                    id="filter_by"
+                    autoHighlight
+                    options={ filterByOption }
+                    sx={ { width: "12.5rem" } }
+                    renderInput={ params => 
+                        <TextField 
+                            {...params} 
+                            label="FILTER BY" 
+                            variant="standard" /> }
+                />
+                <TextField 
+                    id="keyword" 
+                    label="KEYWORD" 
+                    variant="standard"  />
+                <div className="text-fields fd">
                     <TextField 
-                        {...params} 
-                        label="FILTER BY" 
-                        variant="standard" /> }
-            />
-            <TextField 
-                id="keyword" 
-                label="KEYWORD" 
-                variant="standard"  />
-            <div className="text-fields fd">
-                <TextField 
-                    id="start_date" 
-                    type="date" 
-                    label="START DATE" 
-                    variant="standard" 
-                    InputLabelProps={InputLabelProps} />
-                <TextField 
-                    id="end_date" 
-                    type="date" 
-                    label="END DATE" 
-                    variant="standard"
-                    InputLabelProps={InputLabelProps} />
+                        id="start_date" 
+                        type="date" 
+                        label="START DATE" 
+                        variant="standard" 
+                        InputLabelProps={InputLabelProps} />
+                    <TextField 
+                        id="end_date" 
+                        type="date" 
+                        label="END DATE" 
+                        variant="standard"
+                        InputLabelProps={InputLabelProps} />
+                </div>
+                <Button variant="contained" className="-gray -big">Update Data</Button>
             </div>
-            <Button variant="contained" className="-gray -big">Update Data</Button>
         </div>
-    </div>
     )
 }
 
